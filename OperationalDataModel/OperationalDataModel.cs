ec9e9695c4a2e0b14ff0cc9def44358b4f1bf244
kevinwilson <mail@kevinandjuliewilson.com>
2022-01-22 11:26:33 -0500

    Initial check-in of module MetadataRepository

---
+++ OperationalDataModel/OperationalDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace OperationalDataModel
+{
+    public class OperationalDataModel
+    {
+        public OperationalDataModel()
+        {
+        }
+
+        public string Version { get; set; }
+    }
+}
